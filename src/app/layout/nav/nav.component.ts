84a973b8fd45e64ed19b206848b87bf8d6f6253a
Tell Vickers <tcv5000@gmail.com>
2024-08-31 22:38:16 -0400

    swap in angular document service

--- src/app/layout/nav/nav.component.ts
+++ src/app/layout/nav/nav.component.ts
@@ -1,5 +1,5 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
 import { ActivatedRoute, RouterModule } from '@angular/router';
 
@@ -27,18 +27,19 @@ export class NavComponent {
   ];
 
   constructor(
+    @Inject(DOCUMENT) private document: Document,
     private route: ActivatedRoute
   ) {
     this.route.fragment
       .pipe(
         takeUntilDestroyed()
       ).subscribe(fragment => { 
-        // TODO | doesn't work on initial page load w/o setTimeout because document.querySelector is null. Maybe because elements aren't fully rendered yet? idk i h8 javascript
+        // TODO | doesn't work on initial page load w/o setTimeout because document.querySelector is null. Maybe because elements aren't fully rendered yet? idk
         setTimeout(() => {
-          if (fragment && document.querySelector(`#${fragment}`)) {
+          if (fragment) {
             this.currentRoute = fragment;
 
-            document.querySelector(`#${fragment}`)!.scrollIntoView({ behavior: 'smooth' });
+            this.document.querySelector(`#${fragment}`)!.scrollIntoView({ behavior: 'smooth' });
           }
         });
       });
