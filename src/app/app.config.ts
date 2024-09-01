3d9ddb0adc5e177e8c182315e569bd2d77ca5428
Tell Vickers <tcv5000@gmail.com>
2024-08-31 22:51:28 -0400

    hopefully fixed my silly hash strategy bug lmao oops

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { routes } from './app.routes';
 import { provideRouter } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes)
+    provideRouter(routes),
+    { provide: LocationStrategy, useClass: HashLocationStrategy},
   ]
 };
