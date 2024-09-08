cb3f9dadec7ff162199b5a7face5d21ba111c608
Tell Vickers <tcv5000@gmail.com>
2024-09-07 20:34:09 -0400

    remove Sunken Bus show; update EPK photos

--- src/app/shows/shows.constants.ts
+++ src/app/shows/shows.constants.ts
@@ -1,10 +1,12 @@
 import { IShow } from "./shows";
 
-export const SHOWS: IShow[] = [{
-    date: new Date(2024, 8, 7),
-    location: "3312 Evergreen Road, Pittsburgh, PA 15237",
-    startTime: "3pm",
-    imageUrl: "https://files.seatengine.com/talent/headshots/photos/67493/full/data",
-    ticketLink: "https://www.sunkenbus.com/shows/276082",
-    venue: "Red Caiman and Sunken Bus Studios"
-}];
+export const SHOWS: IShow[] = [/*
+    {
+        date: new Date(2024, 8, 7),
+        location: "3312 Evergreen Road, Pittsburgh, PA 15237",
+        startTime: "3pm",
+        imageUrl: "https://files.seatengine.com/talent/headshots/photos/67493/full/data",
+        ticketLink: "https://www.sunkenbus.com/shows/276082",
+        venue: "Red Caiman and Sunken Bus Studios"
+    }
+*/];
