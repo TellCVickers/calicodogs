5ac35594a5dc3435b27966c749582c616f682d69
Tell Vickers <tcv5000@gmail.com>
2025-04-24 19:51:17 -0400

    remove govt center show

--- src/app/shows/shows.constants.ts
+++ src/app/shows/shows.constants.ts
@@ -1,12 +1,6 @@
 import { IShow } from "./shows";
 
-export const SHOWS: IShow[] = [{
-    date: new Date(2025, 3, 17),
-    location: " 715 East St, Pittsburgh, PA 15212",
-    startTime: 'Showtime TBD',
-    otherBands: "Frazé-Frazénko & The Happy Lovers",
-    venue: "The Government Center"
-}
+export const SHOWS: IShow[] = [
     /*
     {
         date: new Date(2024, 11, 30),
