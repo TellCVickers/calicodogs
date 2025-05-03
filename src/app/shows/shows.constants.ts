16ae997f92854d884815624b92496b786462999b
Tell Vickers <tcv5000@gmail.com>
2025-05-03 16:48:47 -0400

    Add millvale and traf shows

--- src/app/shows/shows.constants.ts
+++ src/app/shows/shows.constants.ts
@@ -13,4 +13,18 @@ export const SHOWS: IShow[] = [
         venue: "The Original Pittsburgh Winery"
     }
     */
+   {
+        date: new Date(2025, 4, 17),
+        location: '2 Sedgwick St, Pittsburgh, PA 15209',
+        startTime: '4pm',
+        imageUrl: 'https://static.wixstatic.com/media/7ad06a_239d8c3579a54e3eaef9ce2e85c4be3c~mv2.png/v1/fill/w_729,h_464,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/25-MMF-Website-Top-Image.png',
+        venue: "Millvale Music Festival | Playground Dropout"
+   },
+   {
+        date: new Date(2025, 5, 8),
+        location: '15th Street Plaza @ Waterfront Place',
+        startTime: '2pm',
+        imageUrl: 'https://scontent-lga3-3.xx.fbcdn.net/v/t39.30808-6/494663518_1238531878274283_4526060354115827849_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=127cfc&_nc_ohc=B19yXup-VmMQ7kNvwG7tnN7&_nc_oc=AdkV6RzjVOym0o5D5JetgeSMXt2ks3MzhfY4ERYLJGb815psfHllooP0pVytN7cnsns&_nc_zt=23&_nc_ht=scontent-lga3-3.xx&_nc_gid=RBsARnlGW-Y46_v06d6ZbA&oh=00_AfHHG6o3kApO4aafsZOvxZ8O9czO6RF0MNWxa2ZjqIWH5A&oe=681C6834',
+        venue: "Three Rivers Arts Festival | Dollar Bank Main Stage @ The Strip District"
+   }
 ];
